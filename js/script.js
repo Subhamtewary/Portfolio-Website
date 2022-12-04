a7ab04fa2bc53654ae43fd176276292905893e60
Priyanshu Chaturvedi <119731468+Subhamtewary@users.noreply.github.com>
2022-12-04 19:23:35 +0530

    12

--- js/script.js
+++ js/script.js
@@ -104,30 +104,24 @@ $('.light_mode').click(function(event) {
 if ($(window).width() <= 992) {
 	$('div').removeAttr('data-aos');
 }
+window.addEventListener('load',function(){
+	document.querySelector('body').classList.add("loaded")  
+  });
+  
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/638b83c1daff0e1306dac3e3/1gjcflc12';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  
+  
 
-var typed = new Typed(".mov",{
-    strings: [,  "Ganesh","Content Writer","Web Developer"] ,
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true
 
-    })
-
-	window.addEventListener('load',function(){
-		document.querySelector('body').classList.add("loaded")  
-	  });
-	  
-	  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-	  (function(){
-	  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-	  s1.async=true;
-	  s1.src='https://embed.tawk.to/638b83c1daff0e1306dac3e3/1gjcflc12';
-	  s1.charset='UTF-8';
-	  s1.setAttribute('crossorigin','*');
-	  s0.parentNode.insertBefore(s1,s0);
-	  })();
-	  
-
+	
 
 
 
